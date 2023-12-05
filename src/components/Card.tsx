3a7258c2feb4e7f7b9ab7eead95375dd371c0337
Josue Emaxs <josueemaxsalazar04@gmail.com>
2023-12-05 15:00:01 -0500

    cards and header

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,10 +7,10 @@ interface CardProps {
 const Card = ({ title, text, link }: CardProps) => {
 	return (
 		<a
-			className='block rounded-xl border-2 border-gray-800 px-8 py-4 shadow-xl transition hover:border-[#00ffffc6] hover:shadow-[#00ffff60] hover:scale-105 duration-500'
+			className='block rounded-xl border-2 border-gray-800 px-8 py-4 shadow-xl transition hover:border-[#00ffffc6] hover:shadow-[#00ffff60] hover:scale-105 duration-500 text-white hover:text-[#00fffffb]'
 			href={link}
 		>
-			<h2 className='mt-2 text-xl font-bold text-white text-center'>{title}</h2>
+			<h2 className='mt-2 text-xl font-bold text-center'>{title}</h2>
 
 			<p className='mt-3 text-sm text-gray-300'>{text}</p>
 		</a>
